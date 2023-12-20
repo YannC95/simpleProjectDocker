d860747e810c6ce978a0801a3ad26125f0f3f461
YannC95 <yannm95@hotmail.fr>
2023-12-20 11:38:11 +0100

    init

---
+++ src/main/java/fr/isika/al25/javaGroup/simpleProjectDocker/SimpleProjectDockerApplication.java
@@ -0,0 +1,13 @@
+package fr.isika.al25.javaGroup.simpleProjectDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjectDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProjectDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/isika/al25/javaGroup/simpleProjectDocker/SimpleProjectDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isika.al25.javaGroup.simpleProjectDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
